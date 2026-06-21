4df9c0779b9efed99d145ff6f56be17763fe9beb
CompanyProfile <alanood94895@gmail.com>
2026-06-21 12:14:36 +0400

    create Payment class

--- src/main/java/com/example/fooddelivery/Entities/Payment.java
+++ src/main/java/com/example/fooddelivery/Entities/Payment.java
@@ -1,4 +1,13 @@
 package com.example.fooddelivery.Entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Payment {
 }
