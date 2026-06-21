6f3bf780aacb0d7a1f449168fcd82fa029454fec
CompanyProfile <alanood94895@gmail.com>
2026-06-21 09:49:18 +0400

    Create baseEntity class

---
+++ src/main/java/com/example/fooddelivery/Entities/BaseEntity.java
@@ -0,0 +1,9 @@
+package com.example.fooddelivery.Entities;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+}
--- src/main/java/com/example/fooddelivery/Entities/Customer.java
+++ src/main/java/com/example/fooddelivery/Entities/Customer.java
@@ -1,15 +1,34 @@
 package com.example.fooddelivery.Entities;
 
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.criteria.Order;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@MappedSuperclass
+import java.util.List;
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Customer {
 
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phone;
+    private int passwordHash;
+    private int loyaltyPoints;
+    private int customerCode;
 
+    @OneToMany
+    private List<CustomerAddress> addresses;
+
+    @OneToMany
+    private List<Order> orders;
+
+    @OneToMany
+    private List<Review> reviews;
 }
