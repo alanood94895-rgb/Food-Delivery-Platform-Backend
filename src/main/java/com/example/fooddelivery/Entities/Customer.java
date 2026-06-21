3fb1431c3337a921cfe0d285d42378b656fa7852
CompanyProfile <alanood94895@gmail.com>
2026-06-21 09:38:07 +0400

    Create Customer Class

--- src/main/java/com/example/fooddelivery/Entities/Customer.java
+++ src/main/java/com/example/fooddelivery/Entities/Customer.java
@@ -1,4 +1,15 @@
 package com.example.fooddelivery.Entities;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+
+
 }
