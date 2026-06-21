913330aa204c8c16d607b9855ba02560cdf85a5b
CompanyProfile <alanood94895@gmail.com>
2026-06-21 11:08:57 +0400

    Add relationShip for Restaurant class

--- src/main/java/com/example/fooddelivery/Entities/Restaurant.java
+++ src/main/java/com/example/fooddelivery/Entities/Restaurant.java
@@ -1,13 +1,36 @@
 package com.example.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Restaurant extends BaseEntity {
+
+    private String name;
+    private String description;
+    private String cuisineType;
+    private String openingTime;
+    private String closingTime;
+    private Double minOrderAmount;
+    private Double deliveryFee;
+    private Boolean acceptingOrders;
+
+    @ManyToOne
+    private RestaurantOwner restaurantOwner;
+
+    @OneToMany
+    private List<MenuItem> menuItems;
+
+    @OneToMany
+    private List<ComboMeal> comboMeals;
 }
