7383946bdb505793e3acf3d1a27ff2fcb21ec764
CompanyProfile <alanood94895@gmail.com>
2026-06-21 09:54:28 +0400

    add method for baseEntity class

--- src/main/java/com/example/fooddelivery/Entities/BaseEntity.java
+++ src/main/java/com/example/fooddelivery/Entities/BaseEntity.java
@@ -1,9 +1,27 @@
 package com.example.fooddelivery.Entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    private Boolean isActive = true;
 }
