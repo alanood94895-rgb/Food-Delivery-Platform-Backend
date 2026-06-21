b7bdf1ba2cab03ac21d66f882c556eb7223df5b3
CompanyProfile <alanood94895@gmail.com>
2026-06-21 12:08:17 +0400

    Add relationship for CorporateOrderItem class

--- src/main/java/com/example/fooddelivery/Entities/CorporateOrderItem.java
+++ src/main/java/com/example/fooddelivery/Entities/CorporateOrderItem.java
@@ -1,6 +1,7 @@
 package com.example.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CorporateOrderItem extends BaseEntity {
+    private Integer quantity;
+    private Double unitPrice;
+    private Double totalPrice;
+    private String specialInstructions;
+
+    @ManyToOne
+    private CorporateOrder corporateOrder;
+
+    @ManyToOne
+    private MenuItem menuItem;
+
 }
