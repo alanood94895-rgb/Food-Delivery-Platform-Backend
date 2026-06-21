098fd64310436a25c2c89996899e2e388451ba50
CompanyProfile <alanood94895@gmail.com>
2026-06-21 12:16:59 +0400

    Add relationship for the Payment class

--- src/main/java/com/example/fooddelivery/Entities/Payment.java
+++ src/main/java/com/example/fooddelivery/Entities/Payment.java
@@ -1,13 +1,26 @@
 package com.example.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Payment {
+public class Payment extends BaseEntity {
+
+    private String paymentMethod;
+    private String status;
+    private Double amount;
+    private String transactionRef;
+    private LocalDateTime processedAt;
+
+    @OneToOne
+    private Order order;
+
 }
