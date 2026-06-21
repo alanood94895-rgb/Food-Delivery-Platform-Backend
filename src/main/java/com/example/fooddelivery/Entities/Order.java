9ec7ea33bad9d988eede2cc2d3eece0fe5bece51
CompanyProfile <alanood94895@gmail.com>
2026-06-21 11:53:01 +0400

    Add relationship in order class

--- src/main/java/com/example/fooddelivery/Entities/Order.java
+++ src/main/java/com/example/fooddelivery/Entities/Order.java
@@ -1,13 +1,43 @@
 package com.example.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order extends BaseEntity {
+    private String orderCode;
+    private LocalDateTime orderDate;
+    private String status;
+    private Double subtotal;
+    private Double deliveryFee;
+    private Double discountAmount;
+    private Double totalAmount;
+    private String deliveryNotes;
+
+    @ManyToOne
+    private Customer customer;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    @OneToMany
+    private List<OrderItem> orderItems;
+
+    @OneToOne
+    private Delivery delivery;
+
+    @OneToOne
+    private Payment payment;
+
 }
