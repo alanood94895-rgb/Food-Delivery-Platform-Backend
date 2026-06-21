bd2b4d721e84432fb0729fb84c404fa511f9e293
CompanyProfile <alanood94895@gmail.com>
2026-06-21 11:15:59 +0400

    Add relationship for MenuItem Class

--- src/main/java/com/example/fooddelivery/Entities/MenuItem.java
+++ src/main/java/com/example/fooddelivery/Entities/MenuItem.java
@@ -1,13 +1,29 @@
 package com.example.fooddelivery.Entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MenuItem extends BaseEntity {
+    private String name;
+    private String description;
+    private int price;
+    private Boolean isAvailable;
+    private Boolean isVegetarian;
+    private String calories;
+
+    @ManyToOne
+    private Restaurant restaurant;
+
+    @OneToMany
+    private List <OrderItem> orderItems;
 }
