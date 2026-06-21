71b5e7d5c9efcfee60db7b080f59a97c5f50affd
CompanyProfile <alanood94895@gmail.com>
2026-06-21 15:33:07 +0400

    Modify error

--- src/main/java/com/example/fooddelivery/Entities/Customer.java
+++ src/main/java/com/example/fooddelivery/Entities/Customer.java
@@ -23,12 +23,12 @@ public class Customer extends BaseEntity {
     private Integer loyaltyPoints;
     private String customerCode;
 
-    @OneToMany
+    @OneToMany (mappedBy = "customer")
     private List<CustomerAddress> addresses;
 
-    @OneToMany
+    @OneToMany (mappedBy = "customer")
     private List<Order> orders;
 
-    @OneToMany
+    @OneToMany (mappedBy = "customer")
     private List<Review> reviews;
 }
