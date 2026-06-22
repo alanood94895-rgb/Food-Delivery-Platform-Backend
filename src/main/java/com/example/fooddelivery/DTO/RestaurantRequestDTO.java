192df2c68096aed8e1f4b237e402b77e24151a83
CompanyProfile <alanood94895@gmail.com>
2026-06-22 12:19:53 +0400

    create RestaurantRequestDTO class

---
+++ src/main/java/com/example/fooddelivery/DTO/RestaurantRequestDTO.java
@@ -0,0 +1,29 @@
+package com.example.fooddelivery.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantRequestDTO {
+    @NotBlank
+    private String name;
+
+    private String description;
+
+    @NotBlank
+    private String cuisineType;
+
+    @NotBlank
+    private String openingTime;
+
+    @NotBlank
+    private String closingTime;
+
+    @PositiveOrZero
+    private Double minOrderAmount;
+
+    @PositiveOrZero
+    private Double deliveryFee;
+}
