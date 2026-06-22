5ca3c5800c17ea8dc87a52d66da1e79bfb8ac67a
CompanyProfile <alanood94895@gmail.com>
2026-06-22 11:12:36 +0400

    Create OrderRepository Class

--- src/main/java/com/example/fooddelivery/Repositories/OrderRepository.java
+++ src/main/java/com/example/fooddelivery/Repositories/OrderRepository.java
@@ -1,13 +1,12 @@
 package com.example.fooddelivery.Repositories;
 
-import jakarta.persistence.criteria.Order;
+import com.example.fooddelivery.Entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
