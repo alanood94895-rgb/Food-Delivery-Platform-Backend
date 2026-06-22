37c84ccf33ce02695b3ea0fc958794f83b8b4682
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:22:21 +0400

    Create ReviewRequestDTO class

--- src/main/java/com/example/fooddelivery/DTO/ReviewRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/ReviewRequestDTO.java
@@ -1,6 +1,8 @@
 package com.example.fooddelivery.DTO;
 
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
