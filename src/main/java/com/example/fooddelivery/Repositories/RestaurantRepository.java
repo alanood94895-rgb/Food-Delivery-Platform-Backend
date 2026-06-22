53396604f19f20f99d8e41e4ddb70c150925f603
CompanyProfile <alanood94895@gmail.com>
2026-06-22 09:03:17 +0400

    create RestaurantRepository class

--- src/main/java/com/example/fooddelivery/Repositories/RestaurantRepository.java
+++ src/main/java/com/example/fooddelivery/Repositories/RestaurantRepository.java
@@ -1,4 +1,7 @@
 package com.example.fooddelivery.Repositories;
 
-public interface RestaurantRepository {
+import com.example.fooddelivery.Entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantRepository extends JpaRepository<Customer, Integer> {
 }
