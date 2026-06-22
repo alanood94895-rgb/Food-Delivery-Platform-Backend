ec501b8234b4e779d660212ecb597223a308c337
CompanyProfile <alanood94895@gmail.com>
2026-06-22 12:20:29 +0400

    create RestaurantRequestDTO class

--- src/main/java/com/example/fooddelivery/DTO/RestaurantRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/RestaurantRequestDTO.java
@@ -1,6 +1,7 @@
 package com.example.fooddelivery.DTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
