962719a157ef4fdb63bbdd407edb605ebb7ee9c8
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:21:30 +0400

    Create ReviewRequestDTO class

--- src/main/java/com/example/fooddelivery/DTO/ReviewRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/ReviewRequestDTO.java
@@ -1,4 +1,14 @@
 package com.example.fooddelivery.DTO;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewRequestDTO {
+
+    @Min(1)
+    @Max(5)
+    private Integer rating;
+
+    @NotBlank
+    private String comment;
 }
