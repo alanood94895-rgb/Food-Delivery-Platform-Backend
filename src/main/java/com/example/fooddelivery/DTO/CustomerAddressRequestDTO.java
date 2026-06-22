ca8609b5794ae7a43a6542639ffeeafd7f175810
CompanyProfile <alanood94895@gmail.com>
2026-06-22 12:10:32 +0400

    Create CustomerAddressRequestDTO Class

--- src/main/java/com/example/fooddelivery/DTO/CustomerAddressRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/CustomerAddressRequestDTO.java
@@ -1,5 +1,7 @@
 package com.example.fooddelivery.DTO;
 
+import com.example.fooddelivery.Entities.CustomerAddress;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,4 +10,26 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerAddressRequestDTO {
+    @NotBlank
+    private String street;
+
+    @NotBlank
+    private String city;
+
+    @NotBlank
+    private String building;
+
+    private Boolean isDefault;
+
+    public CustomerAddress toEntity(){
+
+        CustomerAddress address = new CustomerAddress();
+
+        address.setStreet(street);
+        address.setCity(city);
+        address.setBuilding(building);
+        address.setIsDefault(isDefault);
+
+        return address;
+    }
 }
