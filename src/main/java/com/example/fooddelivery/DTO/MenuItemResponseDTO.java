0cb9bc7038240472ed646f121cad4eba7769e799
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:02:59 +0400

    Create MenuItemResponseDTO Class

--- src/main/java/com/example/fooddelivery/DTO/MenuItemResponseDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/MenuItemResponseDTO.java
@@ -8,4 +8,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuItemResponseDTO {
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Boolean isAvailable;
+
+    private Boolean isVegetarian;
+
+    private Integer calories;
 }
