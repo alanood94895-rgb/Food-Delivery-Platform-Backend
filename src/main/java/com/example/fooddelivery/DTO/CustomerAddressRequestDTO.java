c67afe553abe0665917e3d1ef82b696ee1b6af3e
CompanyProfile <alanood94895@gmail.com>
2026-06-22 12:10:09 +0400

    Create CustomerAddressRequestDTO Class

--- src/main/java/com/example/fooddelivery/DTO/CustomerAddressRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/CustomerAddressRequestDTO.java
@@ -1,4 +1,11 @@
 package com.example.fooddelivery.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerAddressRequestDTO {
 }
