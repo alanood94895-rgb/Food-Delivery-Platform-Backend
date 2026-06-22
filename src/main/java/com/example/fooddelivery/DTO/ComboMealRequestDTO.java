91f5bcfa878c175780273590b3369c1d6b31b24b
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:08:37 +0400

    Create ComboMealRequestDTO Class

--- src/main/java/com/example/fooddelivery/DTO/ComboMealRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/ComboMealRequestDTO.java
@@ -1,11 +1,28 @@
 package com.example.fooddelivery.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ComboMealRequestDTO {
+    @NotBlank
+    private String comboName;
+
+    private String description;
+
+    @PositiveOrZero
+    private Double totalPrice;
+
+    private Boolean isAvailable;
+
+    @NotEmpty
+    private List<Integer> menuItemIds;
 }
