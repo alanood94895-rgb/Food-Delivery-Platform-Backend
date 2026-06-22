7fc91894639a66c6fa4eea44474b99e4511881f4
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:19:40 +0400

    Create DriverRequestDTO Class

--- src/main/java/com/example/fooddelivery/DTO/DriverRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/DriverRequestDTO.java
@@ -1,4 +1,30 @@
 package com.example.fooddelivery.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DriverRequestDTO {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9]{8,15}$")
+    private String phone;
+
+    @NotBlank
+    private String passwordHash;
+
+    private String vehicleType;
+
+    private String vehiclePlate;
 }
