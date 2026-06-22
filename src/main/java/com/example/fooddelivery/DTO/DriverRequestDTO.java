9f9b4e117ccb82b5790ad8b22cee56f055be5ee7
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:20:11 +0400

    Create DriverRequestDTO Class

--- src/main/java/com/example/fooddelivery/DTO/DriverRequestDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/DriverRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.fooddelivery.DTO;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
