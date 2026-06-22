f5a094b832beb74e853e2e4d5243ad670db78bf3
CompanyProfile <alanood94895@gmail.com>
2026-06-22 14:23:33 +0400

    Create PaymentResponseDTO Class

--- src/main/java/com/example/fooddelivery/DTO/PaymentResponseDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/PaymentResponseDTO.java
@@ -1,4 +1,17 @@
 package com.example.fooddelivery.DTO;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentResponseDTO {
+
+    private Integer id;
+
+    private String paymentMethod;
+
+    private String status;
+
+    private Double amount;
+
+    private String transactionRef;
 }
