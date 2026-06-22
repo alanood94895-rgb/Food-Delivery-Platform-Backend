44f128f56cd2e5320ab2150bf75f33824758b82c
CompanyProfile <alanood94895@gmail.com>
2026-06-22 10:00:12 +0400

    Create MenuItemRepository class

--- src/main/java/com/example/fooddelivery/Repositories/MenuItemRepository.java
+++ src/main/java/com/example/fooddelivery/Repositories/MenuItemRepository.java
@@ -12,26 +12,17 @@ import java.util.Optional;
 @Repository
 public interface MenuItemRepository extends JpaRepository<MenuItem, Integer> {
 
-    @Query("SELECT m FROM MenuItem m WHERE m.restaurant.id = :id AND m.isActive = true")
-    List<MenuItem> findByRestaurantId(@Param("id") Integer id);
+    @Query("SELECT m FROM MenuItem m " + "WHERE m.restaurant.id = :restaurantId AND m.isActive = true")
+    List<MenuItem> findByRestaurantId(@Param("restaurantId") Integer restaurantId);
 
-    @Query("""
-           SELECT m FROM MenuItem m
-           WHERE m.restaurant.id = :id
-           AND m.isAvailable = true
-           AND m.isActive = true
-           """)
-    List<MenuItem> findByRestaurantIdAndIsAvailableTrue(@Param("id") Integer id);
+    @Query("SELECT m FROM MenuItem m " + "WHERE m.restaurant.id = :restaurantId AND m.isAvailable = true AND m.isActive = true")
+    List<MenuItem> findByRestaurantIdAndIsAvailableTrue(@Param("restaurantId") Integer restaurantId);
 
-    @Query("SELECT m FROM MenuItem m WHERE m.isVegetarian = true AND m.isActive = true")
+    @Query("SELECT m FROM MenuItem m " + "WHERE m.isVegetarian = true AND m.isActive = true")
     List<MenuItem> findByIsVegetarianTrue();
 
-    @Query("""
-           SELECT m FROM MenuItem m
-           WHERE m.price BETWEEN :min AND :max
-           AND m.isActive = true
-           """)
-    List<MenuItem> findByPriceBetween(
-            @Param("min") double min,
-            @Param("max") double max);
+    @Query("SELECT m FROM MenuItem m " + "WHERE m.price BETWEEN :min AND :max AND m.isActive = true")
+    List<MenuItem> findByPriceBetween(@Param("min") double min, @Param("max") double max);
+
+
 }
