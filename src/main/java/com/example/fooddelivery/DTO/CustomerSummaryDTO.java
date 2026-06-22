6bc894939ce9e8aa9cb6217f869bed56cd668d98
CompanyProfile <alanood94895@gmail.com>
2026-06-22 12:07:07 +0400

    Create CustomerSummaryDTO Class

--- src/main/java/com/example/fooddelivery/DTO/CustomerSummaryDTO.java
+++ src/main/java/com/example/fooddelivery/DTO/CustomerSummaryDTO.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.DTO;
 
+import com.example.fooddelivery.Entities.Customer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,4 +9,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerSummaryDTO {
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public static CustomerSummaryDTO fromEntity(Customer customer){
+
+        return new CustomerSummaryDTO(
+                customer.getId(),
+                customer.getFirstName(),
+                customer.getLastName()
+        );
+    }
 }
