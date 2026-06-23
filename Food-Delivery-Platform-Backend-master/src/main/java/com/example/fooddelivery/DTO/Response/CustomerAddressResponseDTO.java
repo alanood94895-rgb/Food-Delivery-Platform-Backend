689280d2c2dfc8ce4a9fde240c790ae1c11bbdb8
Alanoud <alanood94895@gmail.com>
2026-06-23 11:42:38 +0400

    Create CustomerAddressResponseDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/CustomerAddressResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/CustomerAddressResponseDTO.java
@@ -1,6 +1,9 @@
 package com.example.fooddelivery.DTO.Response;
 
 import com.example.fooddelivery.Entities.CustomerAddress;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
