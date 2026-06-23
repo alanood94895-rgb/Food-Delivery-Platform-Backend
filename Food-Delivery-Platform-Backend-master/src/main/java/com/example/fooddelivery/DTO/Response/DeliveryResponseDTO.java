655a185eeb69adc76e2be0a0cfa98ee5f3ed1a39
Alanoud <alanood94895@gmail.com>
2026-06-23 17:45:51 +0400

    create DeliveryResponseDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/DeliveryResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/DeliveryResponseDTO.java
@@ -1,4 +1,41 @@
 package com.example.fooddelivery.DTO.Response;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryResponseDTO {
+    private String trackingCode;
+    private String status;
+    private LocalDateTime assignedAt;
+    private LocalDateTime pickedUpAt;
+    private LocalDateTime deliveredAt;
+
+
+    public static DeliveryResponseDTO fromEntity(Delivery delivery) {
+        if (delivery == null) {
+            return null;
+        }
+
+        DeliveryResponseDTO dto = new DeliveryResponseDTO();
+
+        dto.setTrackingCode(delivery.getTrackingCode());
+        dto.setStatus(delivery.getStatus());
+        dto.setAssignedAt(delivery.getAssignedAt());
+        dto.setPickedUpAt(delivery.getPickedUpAt());
+        delivery.setDeliveredAt(delivery.getDeliveredAt());
+
+        return dto;
+    }
+
+    public static List<DeliveryResponseDTO> fromEntity(List<Delivery> deliveries) {
+        List<DeliveryResponseDTO> dtos = new ArrayList<>();
+        if (deliveries != null) {
+            for (Delivery delivery : deliveries) {
+                dtos.add(fromEntity(delivery));
+            }
+        }
+        return dtos;
+    }
+
+
 }
