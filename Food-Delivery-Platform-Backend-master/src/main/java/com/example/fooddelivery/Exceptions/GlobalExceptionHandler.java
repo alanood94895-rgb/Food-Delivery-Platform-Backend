28dfcacbf3e92baeab89f036d5a782413400ec3a
Alanoud <alanood94895@gmail.com>
2026-06-23 10:06:01 +0400

    Create GlobalExceptionHandler class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/GlobalExceptionHandler.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,7 @@
 package com.example.fooddelivery.Exceptions;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 }
