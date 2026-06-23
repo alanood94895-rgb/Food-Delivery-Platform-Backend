bd0966f3c2ce6db022888b9481859619df0f17d5
Alanoud <alanood94895@gmail.com>
2026-06-23 10:12:53 +0400

    Create InvalidOrderStateException class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/InvalidOrderStateException.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/InvalidOrderStateException.java
@@ -1,4 +1,7 @@
 package com.example.fooddelivery.Exceptions;
 
 public class InvalidOrderStateException extends RuntimeException {
+    public InvalidOrderStateException(String message) {
+        super(message);
+    }
 }
