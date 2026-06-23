9286b8676946b9bb891f1e87d293856f930bdb83
Alanoud <alanood94895@gmail.com>
2026-06-23 11:53:02 +0400

    Create MenuItemResponseDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/MenuItemResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/MenuItemResponseDTO.java
@@ -1,8 +1,11 @@
 package com.example.fooddelivery.DTO.Response;
 
+import com.example.fooddelivery.Entities.MenuItem;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
