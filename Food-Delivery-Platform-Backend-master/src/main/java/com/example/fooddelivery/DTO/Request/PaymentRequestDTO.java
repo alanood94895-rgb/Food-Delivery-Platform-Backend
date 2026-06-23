8fb174018a70279be89408320fe8c9143929a1ae
Alanoud <alanood94895@gmail.com>
2026-06-23 17:35:03 +0400

    create PaymentRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/PaymentRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/PaymentRequestDTO.java
@@ -1,9 +1,13 @@
 package com.example.fooddelivery.DTO.Request;
 
 import com.example.fooddelivery.Entities.Payment;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
