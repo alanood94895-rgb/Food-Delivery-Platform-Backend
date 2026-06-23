8ce0945229c7c2f88344224f511f28a5b286afa9
Alanoud <alanood94895@gmail.com>
2026-06-23 10:44:22 +0400

    Create ErrorResponse class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/ErrorResponse.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/ErrorResponse.java
@@ -3,7 +3,16 @@ package com.example.fooddelivery.Exceptions;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 @Data
 @AllArgsConstructor
 public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private Map<String, String> fieldErrors;
 }
