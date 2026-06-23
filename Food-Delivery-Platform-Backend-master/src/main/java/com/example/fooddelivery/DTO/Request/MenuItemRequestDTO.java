8eb51a669feb0a46e0819e945ab4959e8b6ff791
Alanoud <alanood94895@gmail.com>
2026-06-23 11:27:20 +0400

    Create MenuItemRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/MenuItemRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/MenuItemRequestDTO.java
@@ -1,7 +1,9 @@
 package com.example.fooddelivery.DTO.Request;
 
 import com.example.fooddelivery.Entities.MenuItem;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
