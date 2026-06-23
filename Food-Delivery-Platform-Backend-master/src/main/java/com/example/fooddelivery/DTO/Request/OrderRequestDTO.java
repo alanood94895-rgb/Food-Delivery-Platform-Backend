a024e6c333e385942a577a0760fe0ba4f2a71e57
Alanoud <alanood94895@gmail.com>
2026-06-23 17:33:32 +0400

    Create OrderRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/OrderRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/OrderRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.DTO.Request;
 
+import com.example.fooddelivery.Entities.Order;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
