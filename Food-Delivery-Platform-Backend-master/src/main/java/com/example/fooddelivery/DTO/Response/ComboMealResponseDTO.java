a8627376757b56b94c7db36a6ed0829d7bf43298
Alanoud <alanood94895@gmail.com>
2026-06-23 11:38:52 +0400

    Create ComboMealResponseDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/ComboMealResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/ComboMealResponseDTO.java
@@ -1,4 +1,42 @@
 package com.example.fooddelivery.DTO.Response;
 
+import com.example.fooddelivery.Entities.ComboMeal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ComboMealResponseDTO {
+    private String comboName;
+    private String description;
+    private Double totalPrice;
+    private Boolean isAvailable;
+
+    public static ComboMealResponseDTO fromEntity(ComboMeal comboMeal) {
+        if (comboMeal == null) {
+            return null;
+        }
+
+        ComboMealResponseDTO dto = new ComboMealResponseDTO();
+
+        dto.setComboName(comboMeal.getComboName());
+        dto.setDescription(comboMeal.getDescription());
+        dto.setTotalPrice(comboMeal.getTotalPrice());
+        dto.setIsAvailable(comboMeal.getIsAvailable());
+        return dto;
+    }
+
+    public static List<ComboMealResponseDTO> fromEntity(List<ComboMeal> comboMeals) {
+        List<ComboMealResponseDTO> dtos = new ArrayList<>();
+        if (comboMeals != null) {
+            for (ComboMeal comboMeal : comboMeals) {
+                dtos.add(fromEntity(comboMeal));
+            }
+        }
+        return dtos;
+    }
 }
