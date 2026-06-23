1d1870b72bef11d71d727c7bfb7351ecee9510e8
Alanoud <alanood94895@gmail.com>
2026-06-23 11:23:28 +0400

    Create CustomerRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/CustomerRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/CustomerRequestDTO.java
@@ -31,7 +31,7 @@ public class CustomerRequestDTO {
     private String customerCode;
 
 
-    public Customer toEntity() { //For Creating
+    public Customer toEntity() {
         Customer customer = new Customer();
 
         customer.setFirstName(firstName);
