8d3e213fc7a0169a53e2ed897a9e8a7cf4457d1b
Alanoud <alanood94895@gmail.com>
2026-06-23 11:11:41 +0400

    Create CustomerAddressSummaryDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/CustomerAddressSummaryDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/CustomerAddressSummaryDTO.java
@@ -1,4 +1,24 @@
 package com.example.fooddelivery.DTO.Summary;
 
-public class CustomerAddressSummaryDTO {
+import com.example.fooddelivery.Entities.CustomerAddress;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerAddressSummaryDTO   {
+    private String street;
+    private String city;
+
+    public static CustomerAddressSummaryDTO fromEntity(CustomerAddress customerAddress) {
+
+        CustomerAddressSummaryDTO dto = new CustomerAddressSummaryDTO();
+
+        dto.setStreet(customerAddress.getStreet());
+        dto.setCity(customerAddress.getCity());
+
+        return dto;
+    }
 }
