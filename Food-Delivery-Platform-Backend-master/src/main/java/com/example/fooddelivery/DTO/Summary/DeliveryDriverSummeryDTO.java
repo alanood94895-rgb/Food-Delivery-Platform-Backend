ab9c2f2b2e765219b756fc2b8fe932ca92ae3cc1
Alanoud <alanood94895@gmail.com>
2026-06-23 18:02:16 +0400

    create DeliveryDriverSummeryDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/DeliveryDriverSummeryDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/DeliveryDriverSummeryDTO.java
@@ -1,4 +1,29 @@
 package com.example.fooddelivery.DTO.Summary;
 
+import com.example.fooddelivery.Entities.DeliveryDriver;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryDriverSummeryDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String driverCode;
+    private boolean isOnline;
+
+    public static DeliveryDriverSummeryDTO fromEntity(DeliveryDriver driver) {
+
+        DeliveryDriverSummeryDTO dto = new DeliveryDriverSummeryDTO();
+
+        dto.setFirstName(driver.getFirstName());
+        dto.setLastName(driver.getLastName());
+        dto.setDriverCode(driver.getDriverCode());
+        dto.setOnline(driver.isOnline());
+
+        return dto;
+    }
 }
