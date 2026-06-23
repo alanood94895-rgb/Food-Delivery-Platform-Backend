7180a54f229785b7b4388d023e25bc25fe59fca6
Alanoud <alanood94895@gmail.com>
2026-06-23 11:26:55 +0400

    Create MenuItemRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/MenuItemRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/MenuItemRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.DTO.Request;
 
+import com.example.fooddelivery.Entities.MenuItem;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
@@ -7,20 +8,45 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class MenuItemRequestDTO {
-    @NotBlank
+
+    @NotBlank(message = "Item name is required")
     private String name;
 
+    @NotBlank(message = "Description is required")
     private String description;
 
-    @PositiveOrZero
+    @NotNull(message = "Price is required")
+    @Min(value = 0, message = "Price cannot be negative")
     private Double price;
 
     private Boolean isAvailable;
-
     private Boolean isVegetarian;
-
     private Integer calories;
+
+    @NotNull(message = "Restaurant ID is required")
+    private Long restaurantId;
+
+    public MenuItem toEntity(){
+        MenuItem menuItem = new MenuItem();
+
+        menuItem.setName(name);
+        menuItem.setDescription(description);
+        menuItem.setPrice(price);
+        menuItem.setIsAvailable(isAvailable);
+        menuItem.setIsVegetarian(isVegetarian);
+        menuItem.setCalories(calories);
+
+        return menuItem;
+    }
+
+    public void applyTo(MenuItem menuItem){
+        menuItem.setName(name);
+        menuItem.setDescription(description);
+        menuItem.setPrice(price);
+        menuItem.setIsAvailable(isAvailable);
+        menuItem.setIsVegetarian(isVegetarian);
+        menuItem.setCalories(calories);
+    }
 }
