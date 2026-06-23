edad382745985e60112634fadd57a10245793e6e
Alanoud <alanood94895@gmail.com>
2026-06-23 10:03:57 +0400

    Create DuplicateResourceException class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/DuplicateResourceException.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/DuplicateResourceException.java
@@ -1,4 +1,7 @@
 package com.example.fooddelivery.Exceptions;
 
 public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException (String message){
+
+    }
 }
