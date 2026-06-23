1c48cee978efccc1577e2fddd2b618075a5b456d
Alanoud <alanood94895@gmail.com>
2026-06-23 17:25:42 +0400

    Create DriverRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/DriverRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/DriverRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.fooddelivery.DTO.Request;
 
+import com.example.fooddelivery.Entities.DeliveryDriver;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -12,22 +13,64 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DriverRequestDTO {
 
-    @NotBlank
+    @NotBlank(message = "First name is required")
     private String firstName;
 
-    @NotBlank
+    @NotBlank(message = "Last name is required")
     private String lastName;
 
-    @Email
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
 
-    @Pattern(regexp = "^\\+?[0-9]{8,15}$")
+    @NotBlank(message = "Phone number is required")
     private String phone;
 
-    @NotBlank
+    @NotBlank(message = "Password is required")
     private String passwordHash;
 
+    @NotBlank(message = "Driver code is required")
+    private String driverCode;
+
+    @NotBlank(message = "Vehicle type is required")
     private String vehicleType;
 
+    @NotBlank(message = "Vehicle plate number is required")
     private String vehiclePlate;
+
+    private String currentLate;
+    private String currentLng;
+    private boolean isOnline;
+
+    public DeliveryDriver toEntity() { // For Creating
+        DeliveryDriver driver = new DeliveryDriver();
+
+        driver.setFirstName(firstName);
+        driver.setLastName(lastName);
+        driver.setEmail(email);
+        driver.setPhone(phone);
+        driver.setPasswordHash(passwordHash);
+        driver.setDriverCode(driverCode);
+        driver.setVehicleType(vehicleType);
+        driver.setVehiclePlate(vehiclePlate);
+        driver.setCurrentLate(currentLate);
+        driver.setCurrentLng(currentLng);
+        driver.setOnline(isOnline);
+
+        return driver;
+    }
+
+    public void applyTo(DeliveryDriver driver) { // For Updating
+        driver.setFirstName(firstName);
+        driver.setLastName(lastName);
+        driver.setEmail(email);
+        driver.setPhone(phone);
+        driver.setPasswordHash(passwordHash);
+        driver.setDriverCode(driverCode);
+        driver.setVehicleType(vehicleType);
+        driver.setVehiclePlate(vehiclePlate);
+        driver.setCurrentLate(currentLate);
+        driver.setCurrentLng(currentLng);
+        driver.setOnline(isOnline);
+    }
 }
