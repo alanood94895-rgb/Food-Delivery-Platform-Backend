fd35690b866873a31e9356686f3c69474e8ac9c9
Alanoud <alanood94895@gmail.com>
2026-06-23 18:07:41 +0400

    create PaymentSummeryDTO class

---
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/PaymentSummeryDTO.java
@@ -0,0 +1,28 @@
+package com.example.fooddelivery.DTO.Summary;
+
+import com.example.fooddelivery.Entities.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentSummeryDTO {
+    private String status;
+    private Double amount;
+    private String transactionRef;
+
+    public static PaymentSummeryDTO fromEntity(Payment payment) {
+
+        PaymentSummeryDTO dto = new PaymentSummeryDTO();
+
+        dto.setStatus(payment.getStatus());
+        dto.setAmount(payment.getAmount());
+        dto.setTransactionRef(payment.getTransactionRef());
+
+        return dto;
+    }
+
+
+}
