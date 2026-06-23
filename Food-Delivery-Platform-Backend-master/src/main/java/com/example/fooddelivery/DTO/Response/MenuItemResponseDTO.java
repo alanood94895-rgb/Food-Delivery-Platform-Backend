6c0a40c8af641584de299e3b358d76844c16d8aa
Alanoud <alanood94895@gmail.com>
2026-06-23 11:52:37 +0400

    Create MenuItemResponseDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/MenuItemResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/MenuItemResponseDTO.java
@@ -8,17 +8,39 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MenuItemResponseDTO {
-    private Integer id;
 
     private String name;
-
     private String description;
-
     private Double price;
-
     private Boolean isAvailable;
-
     private Boolean isVegetarian;
-
     private Integer calories;
+
+
+    public static MenuItemResponseDTO fromEntity(MenuItem menuItem) {
+        if (menuItem == null) {
+            return null;
+        }
+
+        MenuItemResponseDTO dto = new MenuItemResponseDTO();
+
+        dto.setName(menuItem.getName());
+        dto.setDescription(menuItem.getDescription());
+        dto.setPrice(menuItem.getPrice());
+        dto.setIsAvailable(menuItem.getIsAvailable());
+        dto.setIsVegetarian(menuItem.getIsVegetarian());
+        dto.setCalories(menuItem.getCalories());
+
+        return dto;
+    }
+
+    public static List<MenuItemResponseDTO> fromEntity(List<MenuItem> menuItems) {
+        List<MenuItemResponseDTO> dtos = new ArrayList<>();
+        if (menuItems != null) {
+            for (MenuItem menuItem : menuItems) {
+                dtos.add(fromEntity(menuItem));
+            }
+        }
+        return dtos;
+    }
 }
