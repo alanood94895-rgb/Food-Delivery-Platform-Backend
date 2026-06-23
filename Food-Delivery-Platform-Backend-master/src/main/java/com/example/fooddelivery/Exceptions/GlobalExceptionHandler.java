b646d1b4593c8f8495799d26a5c509436ff6d86d
Alanoud <alanood94895@gmail.com>
2026-06-23 10:11:47 +0400

    Create InvalidOrderStateException class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/GlobalExceptionHandler.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,16 @@
 package com.example.fooddelivery.Exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
 }
