867c32993de1722fc1a5503f0ef8f5e3de38a13c
Alanoud <alanood94895@gmail.com>
2026-06-23 17:36:16 +0400

    Create RestaurantOwnerRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/RestaurantOwnerRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/RestaurantOwnerRequestDTO.java
@@ -1,4 +1,48 @@
 package com.example.fooddelivery.DTO.Request;
 
+import com.example.fooddelivery.Entities.RestaurantOwner;
+
+@Data
+@NoArgsConstructor
 public class RestaurantOwnerRequestDTO {
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    private String phone;
+
+    @NotBlank(message = "Password is required")
+    private String passwordHash;
+
+    @NotBlank(message = "Business license code is required")
+    private String businessLicenseCode;
+
+    public RestaurantOwner toEntity() { // For Creating
+        RestaurantOwner owner = new RestaurantOwner();
+
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
+        owner.setEmail(email);
+        owner.setPhone(phone);
+        owner.setPasswordHash(passwordHash);
+        owner.setBusinessLicenseCode(businessLicenseCode);
+
+        return owner;
+    }
+
+    public void applyTo(RestaurantOwner owner) { // For Updating
+        owner.setFirstName(firstName);
+        owner.setLastName(lastName);
+        owner.setEmail(email);
+        owner.setPhone(phone);
+        owner.setPasswordHash(passwordHash);
+        owner.setBusinessLicenseCode(businessLicenseCode);
+    }
 }
