d5c7cdc1de9185808f0fcae4607261928eae07c5
Alanoud <alanood94895@gmail.com>
2026-06-23 11:41:55 +0400

    Create CustomerAddressResponseDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/CustomerAddressResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/CustomerAddressResponseDTO.java
@@ -1,4 +1,35 @@
 package com.example.fooddelivery.DTO.Response;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerAddressResponseDTO {
+    private String street;
+    private String city;
+    private String building;
+    private Boolean isDefault;
+
+    public static CustomerAddressResponseDTO fromEntity(CustomerAddress customerAddress) {
+        if (customerAddress == null) {
+            return null;
+        }
+
+        CustomerAddressResponseDTO dto = new CustomerAddressResponseDTO();
+
+        dto.setStreet(customerAddress.getStreet());
+        dto.setCity(customerAddress.getCity());
+        dto.setBuilding(customerAddress.getBuilding());
+        dto.setIsDefault(customerAddress.getIsDefault());
+        return dto;
+    }
+
+    public static List<CustomerAddressResponseDTO> fromEntity(List<CustomerAddress> customerAddresses) {
+        List<CustomerAddressResponseDTO> dtos = new ArrayList<>();
+        if (customerAddresses != null) {
+            for (CustomerAddress customerAddress : customerAddresses) {
+                dtos.add(fromEntity(customerAddress));
+            }
+        }
+        return dtos;
+    }
 }
