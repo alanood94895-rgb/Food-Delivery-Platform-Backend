48c1fe3940c6573f78219f1e045b6a0edcde4340
Alanoud <alanood94895@gmail.com>
2026-06-23 17:39:13 +0400

    Create ReviewRequestDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/ReviewRequestDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Request/ReviewRequestDTO.java
@@ -1,11 +1,15 @@
 package com.example.fooddelivery.DTO.Request;
 
+import com.example.fooddelivery.Entities.Review;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
