753d47ae388d3d9ac7d3d0d607b105fe74f79889
Alanoud <alanood94895@gmail.com>
2026-06-23 18:03:04 +0400

    create DeliverySummeryDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/DeliverySummaryDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/DeliverySummaryDTO.java
@@ -2,6 +2,21 @@ package com.example.fooddelivery.DTO.Summary;
 
 import com.example.fooddelivery.Entities.Delivery;
 
-public class DeliverySummaryDTO {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliverySummeryDTO {
+    private String trackingCode;
+    private String status;
+
+    public static DeliverySummeryDTO fromEntity(Delivery delivery) {
+
+        DeliverySummeryDTO dto = new DeliverySummeryDTO();
+
+        dto.setTrackingCode(delivery.getTrackingCode());
+        dto.setStatus(delivery.getStatus());
+
+        return dto;
+    }
 
 }
