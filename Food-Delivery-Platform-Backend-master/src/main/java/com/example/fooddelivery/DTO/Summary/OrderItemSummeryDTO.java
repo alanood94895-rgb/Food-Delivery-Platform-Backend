86d0e463c30cc99bada501b880da8b6d390270ae
Alanoud <alanood94895@gmail.com>
2026-06-23 18:05:57 +0400

    create OrderSummeryDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/OrderItemSummeryDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/OrderItemSummeryDTO.java
@@ -1,4 +1,24 @@
 package com.example.fooddelivery.DTO.Summary;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemSummeryDTO {
+    private Long id;
+    private Integer quantity;
+    private Double totalPrice;
+
+    public static OrderItemSummeryDTO fromEntity(OrderItem orderItem) {
+
+
+        OrderItemSummeryDTO dto = new OrderItemSummeryDTO();
+        dto.setQuantity(orderItem.getQuantity());
+        dto.setTotalPrice(orderItem.getTotalPrice());
+
+        return dto;
+    }
 }
