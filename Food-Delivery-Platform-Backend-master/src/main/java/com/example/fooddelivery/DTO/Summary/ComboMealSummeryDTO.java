bdf7d0c9e7a7f85be7038deea2e1b2c4121ef36d
Alanoud <alanood94895@gmail.com>
2026-06-23 11:05:26 +0400

    Create ComboMealSummeryDTO class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/ComboMealSummeryDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Summary/ComboMealSummeryDTO.java
@@ -1,4 +1,27 @@
 package com.example.fooddelivery.DTO.Summary;
 
+import com.example.fooddelivery.Entities.ComboMeal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ComboMealSummeryDTO {
+
+    private String comboName;
+    private Double totalPrice;
+    private Boolean isAvailable;
+
+    public static ComboMealSummeryDTO fromEntity(ComboMeal comboMeal) {
+
+        ComboMealSummeryDTO dto = new ComboMealSummeryDTO();
+
+        dto.setComboName(comboMeal.getComboName());
+        dto.setTotalPrice(comboMeal.getTotalPrice());
+        dto.setIsAvailable(comboMeal.getIsAvailable());
+
+        return dto;
+    }
 }
