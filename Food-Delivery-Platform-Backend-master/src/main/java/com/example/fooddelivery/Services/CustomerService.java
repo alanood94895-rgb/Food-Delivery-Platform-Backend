1ef29338b8867234598e3f1c638613ad95c48935
Alanoud <alanood94895@gmail.com>
2026-06-24 09:17:44 +0400

    create CustomerService class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/CustomerService.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/CustomerService.java
@@ -1,7 +1,21 @@
 package com.example.fooddelivery.Services;
 
+import com.example.fooddelivery.DTO.Request.CustomerRequestDTO;
+import com.example.fooddelivery.DTO.Response.CustomerResponseDTO;
+import com.example.fooddelivery.Repositories.CustomerAddressRepository;
+import com.example.fooddelivery.Repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CustomerAddressRepository customerAddressRepository;
+
+    public CustomerResponseDTO createCustomer (CustomerRequestDTO dto){
+
+    }
 }
