2ea406e74bbebdeb7d7e0d76de1e2745d58ac401
Alanoud <alanood94895@gmail.com>
2026-06-24 14:24:55 +0400

    create RestaurantRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ComboMealRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ComboMealRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 @Repository
 public interface ComboMealRepository extends JpaRepository <ComboMeal, Integer> {
-    @Query("SELECT c FROM ComboMeal c " + "WHERE c.restaurant.id = :restaurantId AND c.isActive = true")
+    @Query("SELECT c FROM ComboMeal c " +
+            "WHERE c.restaurant.id = :restaurantId AND c.isActive = true")
     List<ComboMeal> findByRestaurantId(@Param("restaurantId") Integer restaurantId);
 
-    @Query("SELECT c FROM ComboMeal c " + "JOIN c.menuItems m " + "WHERE m.id = :menuItemId AND c.isActive = true")
+    @Query("SELECT c FROM ComboMeal c " + "JOIN c.menuItemList m " + "WHERE m.id = :menuItemId AND c.isActive = true")
     List<ComboMeal> findCombosContainingMenuItem(@Param("menuItemId") Integer menuItemId);
-
 }
