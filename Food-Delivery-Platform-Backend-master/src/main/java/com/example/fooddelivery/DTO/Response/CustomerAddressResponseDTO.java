b8a4f6356bfb36c3727b91facf506fc4e9c4701a
Alanoud <alanood94895@gmail.com>
2026-06-24 10:21:09 +0400

    solve the error in CustomerAddressResponseDTO

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/CustomerAddressResponseDTO.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/DTO/Response/CustomerAddressResponseDTO.java
@@ -5,39 +5,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerAddressResponseDTO {
+    private int id;
     private String street;
     private String city;
     private String building;
-    private Boolean isDefault;
+    private boolean isDefault;
+    private boolean isActive;
 
     public static CustomerAddressResponseDTO fromEntity(CustomerAddress customerAddress) {
-        if (customerAddress == null) {
-            return null;
-        }
-
         CustomerAddressResponseDTO dto = new CustomerAddressResponseDTO();
-
+        dto.setId(customerAddress.getId());
         dto.setStreet(customerAddress.getStreet());
         dto.setCity(customerAddress.getCity());
         dto.setBuilding(customerAddress.getBuilding());
-        dto.setIsDefault(customerAddress.getIsDefault());
-        return dto;
-    }
+        dto.setDefault(customerAddress.isDefault());
+        dto.setActive(customerAddress.isActive());
 
-    public static List<CustomerAddressResponseDTO> fromEntity(List<CustomerAddress> customerAddresses) {
-        List<CustomerAddressResponseDTO> dtos = new ArrayList<>();
-        if (customerAddresses != null) {
-            for (CustomerAddress customerAddress : customerAddresses) {
-                dtos.add(fromEntity(customerAddress));
-            }
-        }
-        return dtos;
+        return dto;
     }
 }
