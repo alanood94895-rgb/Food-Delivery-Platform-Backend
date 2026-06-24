ac81bca4de90bb6c241fd8a4ff09fddea7d760e4
Alanoud <alanood94895@gmail.com>
2026-06-24 14:47:07 +0400

    create CustomerController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/CustomerController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/CustomerController.java
@@ -1,13 +1,13 @@
 package com.example.fooddelivery.Controllers;
 
+import com.example.fooddelivery.DTO.Request.CustomerRequestDTO;
 import com.example.fooddelivery.DTO.Response.CustomerResponseDTO;
 import com.example.fooddelivery.Services.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customers")
