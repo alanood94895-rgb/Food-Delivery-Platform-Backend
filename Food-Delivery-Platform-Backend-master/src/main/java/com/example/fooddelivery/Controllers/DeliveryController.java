0adebe0f18e64f859e994e02d7f37d4c28dfd1e6
Alanoud <alanood94895@gmail.com>
2026-06-24 15:36:37 +0400

    create DeliveryController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/DeliveryController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/DeliveryController.java
@@ -3,9 +3,8 @@ package com.example.fooddelivery.Controllers;
 import com.example.fooddelivery.DTO.Response.DeliveryResponseDTO;
 import com.example.fooddelivery.Services.DeliveryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/deliveries")
