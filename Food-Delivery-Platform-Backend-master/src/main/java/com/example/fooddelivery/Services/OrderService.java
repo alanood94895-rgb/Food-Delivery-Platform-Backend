d9cccb46577e0c2db53e6ccdb23f2e20bcc16915
Alanoud <alanood94895@gmail.com>
2026-06-24 11:58:29 +0400

    create  OrderService class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/OrderService.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/OrderService.java
@@ -3,6 +3,7 @@ package com.example.fooddelivery.Services;
 import com.example.fooddelivery.DTO.Request.OrderItemRequestDTO;
 import com.example.fooddelivery.DTO.Response.OrderResponseDTO;
 import com.example.fooddelivery.Entities.Customer;
+import com.example.fooddelivery.Entities.Order;
 import com.example.fooddelivery.Entities.Restaurant;
 import com.example.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.example.fooddelivery.Repositories.*;
