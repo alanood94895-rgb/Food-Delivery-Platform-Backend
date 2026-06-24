f861b567625ff4b3f0499ad88c1ca0aa5e82ac30
Alanoud <alanood94895@gmail.com>
2026-06-24 14:52:39 +0400

    create DriverController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/DriverController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/DriverController.java
@@ -1,5 +1,10 @@
 package com.example.fooddelivery.Controllers;
 
+import com.example.fooddelivery.DTO.Response.DeliveryDriverResponseDTO;
+import com.example.fooddelivery.Services.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
