3a80d62cd99ebb5522fbb98cbbe6bfb080e65368
Alanoud <alanood94895@gmail.com>
2026-06-24 11:58:40 +0400

    create  OrderService class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/OrderService.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/OrderService.java
@@ -10,6 +10,8 @@ import com.example.fooddelivery.Repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class OrderService {
     OrderRepository orderRepository;
