ff59cefc5e1ed0404b5fae8a139237beef536a12
Alanoud <alanood94895@gmail.com>
2026-06-24 14:58:24 +0400

    create OrderController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/OrderController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/OrderController.java
@@ -1,5 +1,12 @@
 package com.example.fooddelivery.Controllers;
 
+import com.example.fooddelivery.DTO.Response.OrderResponseDTO;
+import com.example.fooddelivery.Services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
