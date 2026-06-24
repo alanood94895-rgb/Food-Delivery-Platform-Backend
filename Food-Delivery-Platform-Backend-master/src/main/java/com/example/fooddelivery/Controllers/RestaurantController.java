d13c6cf43196ccf0603bacaff6e4471baddf67b7
Alanoud <alanood94895@gmail.com>
2026-06-24 15:33:07 +0400

    create OrderController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/RestaurantController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/RestaurantController.java
@@ -1,15 +1,19 @@
 package com.example.fooddelivery.Controllers;
 
+import com.example.fooddelivery.DTO.Request.ComboMealRequestDTO;
+import com.example.fooddelivery.DTO.Request.MenuItemRequestDTO;
+import com.example.fooddelivery.DTO.Request.RestaurantRequestDTO;
+import com.example.fooddelivery.DTO.Response.ComboMealResponseDTO;
+import com.example.fooddelivery.DTO.Response.MenuItemResponseDTO;
 import com.example.fooddelivery.DTO.Response.RestaurantResponseDTO;
 import com.example.fooddelivery.Services.RestaurantService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/restaurants")
