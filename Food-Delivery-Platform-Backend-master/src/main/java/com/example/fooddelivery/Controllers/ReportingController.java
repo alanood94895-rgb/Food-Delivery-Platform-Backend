f6a3c365f21d7421a24409dfcf187a0809f709a2
Alanoud <alanood94895@gmail.com>
2026-06-25 10:00:26 +0400

    create ReportingController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/ReportingController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/ReportingController.java
@@ -5,6 +5,7 @@ import com.example.fooddelivery.Services.DeliveryService;
 import com.example.fooddelivery.Services.OrderService;
 import com.example.fooddelivery.Services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,20 @@ public class ReportingController {
         this.orderService = orderService;
         this.customerService = customerService;
         this.deliveryService = deliveryService;
+    }
+
+    @GetMapping("/revenue/restaurant/{restaurantId}")
+    public ResponseEntity<Double> getRestaurantRevenue(@PathVariable Integer restaurantId, @RequestParam Date date) {
+        return ResponseEntity.ok(restaurantService.getRestaurantRevenue(restaurantId, date));
+    }
+
+    @GetMapping("/orders/count/restaurant/{restaurantId}")
+    public ResponseEntity<Long> getRestaurantOrderCount(@PathVariable Integer restaurantId) {
+        return ResponseEntity.ok(restaurantService.getRestaurantOrderCount(restaurantId));
+    }
+
+    @GetMapping("/customers/topLoyalty")
+    public ResponseEntity<List<CustomerResponseDTO>> getTopLoyalCustomers() {
+        return ResponseEntity.ok(Collections.singletonList(customerService.getTopLoyalCustomers()));
     }
 }
