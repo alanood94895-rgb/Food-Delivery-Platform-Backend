4ebe58e6c91225ad9842a11e014c7ca33c19cab4
Alanoud <alanood94895@gmail.com>
2026-06-25 20:43:43 +0400

    create PaymentRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/PaymentRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/PaymentRepository.java
@@ -1,10 +1,23 @@
 package com.example.fooddelivery.Repositories;
 
 import com.example.fooddelivery.Entities.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Date;
+
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
-    @Query("select p from Pyment p where p.order.orderCode=:orderId")
+    @Query("select p from Payment p where p.order.orderCode=:orderId")
     public Payment findByOrderId(Integer orderId);
+    @Query("""
+       select p
+       from Payment p
+       where p.isActive=true
+       and p.paymentMethod =:method
+       and p.status =:status
+       and p.createdDate between :from and :to
+       """)
+    Page<Payment> searchPayments(String method, String status, Date from, Date to, Pageable pageable);
 }
