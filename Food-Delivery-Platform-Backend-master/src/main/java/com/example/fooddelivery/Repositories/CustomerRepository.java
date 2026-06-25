a7e6d1349f2721a9877315e8d72036dceb4cfaf3
Alanoud <alanood94895@gmail.com>
2026-06-25 10:49:43 +0400

    create CustomerRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/CustomerRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.example.fooddelivery.Repositories;
 
 import com.example.fooddelivery.DTO.Response.CustomerResponseDTO;
 import com.example.fooddelivery.Entities.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,17 +17,17 @@ import java.util.List;
 public interface CustomerRepository extends JpaRepository <Customer, Integer> {
 
     @Query("SELECT c from Customer c WHERE c.isActive=true AND c.email=:email")
-    Customer findByEmail(@Param("email") String email);
+    Customer  findByEmail(@Param("email") String email);
 
     @Query("Select c from Customer c where c.isActive=true and c.loyaltyPoints>=:points")
     List<Customer> findByLoyaltyPointsGreaterThanEqual(@Param("points") int points);
-
     @Query("SELECT c FROM Customer c WHERE c.isActive=true AND c.createdDate BETWEEN :createdDate AND CURRENT_TIMESTAMP")
     List<Customer> CustomerRegisterWithDateRange(@Param("createdDate") Date createdDate);
-
     @Query("select c from Customer c where c.isActive=true")
     List<CustomerResponseDTO> getAllCustomers();
-
     @Query(value = " select c from Customer c where c.isActive=true order by c.loyaltyPoints desc limit 10")
     List<Customer> getTopLoyalCustomers();
+    @Query("select c from Customer c where c.isActive=true and c.firstName=:nam ")
+    Page<Customer> findByFirstName(String name, Pageable pageable);
+
 }
--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/CustomerService.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/CustomerService.java
@@ -12,6 +12,9 @@ import com.example.fooddelivery.Exceptions.ResourceNotFoundException;
 import com.example.fooddelivery.Repositories.CustomerAddressRepository;
 import com.example.fooddelivery.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -151,5 +154,9 @@ public class CustomerService {
     }
     public CustomerResponseDTO getTopLoyalCustomers( ) {
         return (CustomerResponseDTO) customerRepository.getTopLoyalCustomers();
+    }
+    public Page<CustomerResponseDTO> searchCustomers(String name, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return customerRepository.findByFirstName(name,pageable).map(CustomerResponseDTO::fromEntity);
     }
 }
