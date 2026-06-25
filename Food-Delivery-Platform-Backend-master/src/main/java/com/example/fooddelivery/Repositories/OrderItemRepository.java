367ec20f90cb76613459e9d41c296f7d0b695087
Alanoud <alanood94895@gmail.com>
2026-06-25 20:41:37 +0400

    create OrderItemRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/OrderItemRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/OrderItemRepository.java
@@ -2,6 +2,12 @@ package com.example.fooddelivery.Repositories;
 
 import com.example.fooddelivery.Entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem,Integer> {
+    @Query(" select oi from OrderItem oi where oi.order.restaurant.id=:restaurantId order by oi.quantity desc")
+    List<OrderItem> getTopSellingItems(Integer restaurantId);
+
 }
