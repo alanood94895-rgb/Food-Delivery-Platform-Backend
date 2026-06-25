8aa09012017b3a8eedd8d08697caa4198fbc229b
Alanoud <alanood94895@gmail.com>
2026-06-25 09:02:50 +0400

    create PaymentController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/PaymentController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/PaymentController.java
@@ -1,6 +1,11 @@
 package com.example.fooddelivery.Controllers;
 
 import com.example.fooddelivery.DTO.Response.PaymentResponseDTO;
+import com.example.fooddelivery.Services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
