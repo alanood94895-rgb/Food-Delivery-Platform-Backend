685f1ea39dce4d161d30d8f2e48ea2235371cb50
Alanoud <alanood94895@gmail.com>
2026-06-25 09:47:10 +0400

    create ReportingController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/ReportingController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/ReportingController.java
@@ -1,4 +1,23 @@
 package com.example.fooddelivery.Controllers;
 
+import com.example.fooddelivery.Services.RestaurantService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reports")
 public class ReportingController {
+    RestaurantService restaurantService;
+    OrderService orderService;
+    CustomerService customerService;
+    DeliveryService deliveryService;
+
+    @Autowired
+    public ReportingController(RestaurantService restaurantService, OrderService orderService,
+                               CustomerService customerService, DeliveryService deliveryService) {
+        this.restaurantService = restaurantService;
+        this.orderService = orderService;
+        this.customerService = customerService;
+        this.deliveryService = deliveryService;
+    }
 }
