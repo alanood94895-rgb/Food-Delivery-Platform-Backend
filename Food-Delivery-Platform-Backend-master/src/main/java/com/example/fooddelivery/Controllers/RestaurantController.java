acf6777fe77352ee602fc9a8063ce627add7f632
Alanoud <alanood94895@gmail.com>
2026-06-25 21:01:21 +0400

    create RestaurantController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/RestaurantController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/RestaurantController.java
@@ -78,4 +78,13 @@ public class RestaurantController {
         restaurantService.bulkUpdateMenuItemPrices(id, percentage);
         return ResponseEntity.noContent().build();
     }
+    @GetMapping("/{id}/menu/topSellers")
+    public ResponseEntity<List<MenuItemResponseDTO>> getTopSellingItems(@PathVariable Integer id) {
+        return ResponseEntity.ok(restaurantService.getTopSellingItems(id));
+    }
+    @GetMapping("/menu/search")
+    public ResponseEntity<List<MenuItemResponseDTO>> searchMenuItems(@RequestParam String keyword, @RequestParam double minCalories,
+                                                                     @RequestParam double maxCalories) {
+        return ResponseEntity.ok(restaurantService.searchMenuItems(keyword, minCalories, maxCalories));
+    }
 }
