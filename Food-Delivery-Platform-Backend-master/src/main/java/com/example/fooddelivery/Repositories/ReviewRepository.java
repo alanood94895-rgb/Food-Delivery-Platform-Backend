ab84659281fe7f4932e3bfc4394f00d71820594f
Alanoud <alanood94895@gmail.com>
2026-06-25 09:10:46 +0400

    create ReviewRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ReviewRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ReviewRepository.java
@@ -2,6 +2,14 @@ package com.example.fooddelivery.Repositories;
 
 import com.example.fooddelivery.Entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review,Integer> {
+
+    @Query("select r from Review r where r.restaurant.isActive=true and r.restaurant.id=:restaurantId")
+    List<Review> findByRestaurantIdAndIsActiveTrue(Integer restaurantId);
+    @Query("select r from Review r where r.deliveryDriver.isActive=true and r.deliveryDriver.driverCode=:driverId")
+    List<Review> findByDeliveryDriverIdAndIsActiveTrue(Integer driverId);
 }
