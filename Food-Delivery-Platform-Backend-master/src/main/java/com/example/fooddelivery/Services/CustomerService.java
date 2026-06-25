eb8452f4d9888c96221b382c0e24699711babd0c
Alanoud <alanood94895@gmail.com>
2026-06-25 10:02:18 +0400

    create CustomerService class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/CustomerService.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Services/CustomerService.java
@@ -149,4 +149,7 @@ public class CustomerService {
         }
         return orderResponseList;
     }
+    public CustomerResponseDTO getTopLoyalCustomers( ) {
+        return (CustomerResponseDTO) customerRepository.getTopLoyalCustomers();
+    }
 }
