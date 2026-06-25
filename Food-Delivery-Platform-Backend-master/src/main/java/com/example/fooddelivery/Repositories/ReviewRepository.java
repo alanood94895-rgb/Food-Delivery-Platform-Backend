f044b989b36bd591811d165c87dead2f5fb619a2
Alanoud <alanood94895@gmail.com>
2026-06-25 20:47:09 +0400

    create ReviewRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ReviewRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.example.fooddelivery.Repositories;
 
 import com.example.fooddelivery.Entities.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
