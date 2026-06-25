30e1a51420fae56771bbca00b39599b259417606
Alanoud <alanood94895@gmail.com>
2026-06-25 09:09:01 +0400

    create ReviewController class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/ReviewController.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Controllers/ReviewController.java
@@ -1,6 +1,10 @@
 package com.example.fooddelivery.Controllers;
 
 import com.example.fooddelivery.DTO.Response.ReviewResponseDTO;
+import com.example.fooddelivery.Services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
