fb24ec704648bb8c3a90f2256cd35af5cc5371b4
Alanoud <alanood94895@gmail.com>
2026-06-25 20:46:41 +0400

    create ReviewRepository class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ReviewRepository.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Repositories/ReviewRepository.java
@@ -9,7 +9,17 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review,Integer> {
 
     @Query("select r from Review r where r.restaurant.isActive=true and r.restaurant.id=:restaurantId")
-    List<Review> findByRestaurantIdAndIsActiveTrue(Integer restaurantId);
+    List<Review> findByRestaurantIdAndIsActiveTrue(@Param("restaurantId") Integer restaurantId);
+
     @Query("select r from Review r where r.deliveryDriver.isActive=true and r.deliveryDriver.driverCode=:driverId")
-    List<Review> findByDeliveryDriverIdAndIsActiveTrue(Integer driverId);
+    List<Review> findByDeliveryDriverIdAndIsActiveTrue(@Param("driverId") Integer driverId);
+
+    @Query(" select avg(r.rating) from Review r where r.restaurant.id =:restaurantId and r.isActive=true")
+    Double getRestaurantAverage(@Param("restaurantId") Integer restaurantId);
+
+    @Query(" select avg(r.rating) from Review r where r.deliveryDriver.driverCode =:driverId and r.isActive = true")
+    Double getDriverAverage(@Param("driverId") Integer driverId);
+
+    @Query("select r from Review r where r.restaurant.isActive=true and r.restaurant.id=:restaurantId")
+    Page<Review> findByRestaurantIdAndIsActiveTrue(@Param("restaurantId") Integer restaurantId, Pageable pageable);
 }
