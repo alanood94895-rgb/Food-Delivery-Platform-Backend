b5c1a9f9e8582274a154103677a2d8500aefcbfa
Alanoud <alanood94895@gmail.com>
2026-06-26 11:02:23 +0400

    create BaseEntity class

--- Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Entities/BaseEntity.java
+++ Food-Delivery-Platform-Backend-master/src/main/java/com/example/fooddelivery/Entities/BaseEntity.java
@@ -1,4 +1,20 @@
 package com.example.fooddelivery.Entities;
 
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+    private Boolean isActive = true;
 }
